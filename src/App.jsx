606fc4a4d1e79faf8d22df28a89f9422ad0fdd81
an <anny880811@gmail.com>
2026-06-07 17:55:35 +0800

    feat : 建立表單基本結構與樣式

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="form-container mt-5">
+        <h2 className="title mt-3">表單測試</h2>
+        <div className="form-custom">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
